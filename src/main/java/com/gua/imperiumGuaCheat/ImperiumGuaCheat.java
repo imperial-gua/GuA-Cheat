99e71caa36f03ed01495b7f9f4eefc310fb9e9fb
oxxx1mif <pluginexteragram@gmail.com>
2026-02-24 21:39:01 +1000

    create

---
+++ src/main/java/com/gua/imperiumGuaCheat/ImperiumGuaCheat.java
@@ -0,0 +1,30 @@
+/*
+    This file is part of ImperiumCheat.
+
+    ImperiumCheat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImperiumCheat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with ImperiumCheat.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2026 Impergram & ImpergramX & GuA development team
+              2026 Obitocjkiy Gleb || oxxx1mif || oxxx1Dev
+*/
+
+package com.gua.imperiumGuaCheat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ImperiumGuaCheat implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
